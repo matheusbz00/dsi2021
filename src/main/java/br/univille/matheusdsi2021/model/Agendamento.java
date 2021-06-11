cd31633bdb8c187b18aedbd6df80884eb668d18c
matheusbz00 <matheusbz000@hotmail.com>
2021-06-11 00:09:00 +0000

    first commit

--- src/main/java/br/univille/matheusdsi2021/model/Agendamento.java
+++ src/main/java/br/univille/matheusdsi2021/model/Agendamento.java
@@ -1,10 +1,16 @@
 package br.univille.matheusdsi2021.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Date;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Agendamento {
@@ -13,6 +19,11 @@ public class Agendamento {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String descricao;
+
+    @ManyToMany(cascade = CascadeType.REFRESH)
+    @JoinColumn(name="usuario_id")
+    private List<Usuario> usuario = 
+        new ArrayList<Usuario>();
     
     public long getId() {
         return id;
---
+++ src/main/java/br/univille/matheusdsi2021/model/README.md
@@ -0,0 +1,8 @@
+# Projeto de DSI 2021
+
+
+
+## Como enviar meu projeto para o Github
+`git add .`
+`git commit -m "descricao do que foi feito"`
+`git push -u origin main`
