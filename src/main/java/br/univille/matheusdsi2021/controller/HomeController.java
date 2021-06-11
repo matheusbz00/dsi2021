af43e8b19bf08d7fa7f7652bf920dc248097f2ad
matheusbz00 <matheusbz000@hotmail.com>
2021-06-11 01:30:51 +0000

    incluindo bootstrap

---
+++ src/main/java/br/univille/matheusdsi2021/controller/HomeController.java
@@ -0,0 +1,26 @@
+package br.univille.matheusdsi2021.controller;
+
+import java.util.HashMap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public ModelAndView index(){
+
+        HashMap<String,String> listaDeParzinho = new HashMap<>();
+        listaDeParzinho.put("nome","Sistema de DSI 2021");
+        listaDeParzinho.put("nomeEmpresa","UNIVILLE SISTEMAS");
+        
+
+        return new ModelAndView("home/index",listaDeParzinho);
+
+    }
+    
+}
